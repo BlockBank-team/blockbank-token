5d0f9a2989f5da049fec7e88912711fbfbc408c9
Jack Li <jackz163299@gmail.com>
2021-04-26 05:49:39 -0400

    change token symbol BANK to BBANK

--- contracts/BlockBank.sol
+++ contracts/BlockBank.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.7.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BlockBank is ERC20("BlockBank", "BANK"), Ownable {
+contract BlockBank is ERC20("BlockBank", "BBANK"), Ownable {
     
     uint public constant TOTAL_SUPPLY = 400000000 * (10 ** 18);
     
